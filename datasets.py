947ca1aac067fd79c8fd72cd6dc24dd9a2af0fed
Monyverse <geomallapp@gmail.com>
2024-02-27 17:41:42 +0000

    dataloaders

---
+++ datasets.py
@@ -0,0 +1,14 @@
+mport polars as pl
+
+from datasets import DatasetsLoader
+
+# Usage example:
+loader = DatasetsLoader()
+
+df = loader.load('tvl-fee-per-protocol')
+print(df)  # This will print a LazyFrame object
+
+# If you want to collect the results into a DataFrame, you can call .collect()
+if not isinstance(df, pl.DataFrame):
+    df = df.collect()
+print(df)
